f68ddbc1021ddc3b6caeb2749a83ee3130bcda5a
neomat-prog <thegreyliongaming@gmail.com>
2026-07-30 22:16:53 +0200

    feat: implement main() go routine run

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,1 +1,31 @@
-package tracker
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/neomat-prog/internal/market"
+	"github.com/neomat-prog/internal/tracker"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	tp := market.NewMockTransport("BTCUSDT", 100*time.Millisecond, 0)
+	t := tracker.NewTracker(tracker.TrackerOpts{
+		Symbols:  []string{"BTCUSDT"},
+		RingSize: 100,
+		Interval: 500 * time.Millisecond,
+	}, tp)
+
+	go func() {
+		<-ctx.Done()
+		tp.Close()
+		t.Stop()
+	}()
+
+	t.Start()
+}
